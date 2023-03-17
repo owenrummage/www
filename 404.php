164af0eabeb0ca556517190fbf4e7b477d9fcc37
Owen Rummage <owen@rummage.cc>
2023-03-17 16:54:59 -0500

    add 404.php for beta deployment

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+
+
+<html>
+	<head>
+		<title>404 Page not Found</title>
+	</head>
+
+	<body style="width:100%;height:100%;display: flex;align-items:center;justify-content:center;">
+		<div style="display: flex;flex-direction:column;justify-content:center;">
+<pre style="margin-left: auto; margin-right: auto;margin-bottom:0;">
+ The page you were looking for cannot be found! (404)
+------------------------------------------------------
+</pre>
+<pre style="margin-top:0;">
+Usually there would be quotes here, but Jared broke the server
+</pre>
+		</div>
+	</body>
+</html>
