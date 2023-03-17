81033c4dfbe05da5604260eeb8abe9ae7b5cad5a
Owen Rummage <owen@rummage.cc>
2023-03-17 16:52:59 -0500

    TODO: Interactive Data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # www
 My personal website, now with PHP
+
+You can go see it [here](https://rummage.cc)
---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+		<title>Owen Rummage - Home</title>
+	</head>
+
+	<body>
+		<div style="display:flex;flex-direction:column;width:100%;height:100%;align-items:center;">
+<pre>
+=====================================================================================
+*     ____                         ____                                             *
+*    / __ \_      _____  ____     / __ \__  ______ ___  ____ ___  ____ _____ ____   *
+*   / / / / | /| / / _ \/ __ \   / /_/ / / / / __ `__ \/ __ `__ \/ __ `/ __ `/ _ \  *
+*  / /_/ /| |/ |/ /  __/ / / /  / _, _/ /_/ / / / / / / / / / / / /_/ / /_/ /  __/  *
+*  \____/ |__/|__/\___/_/ /_/  /_/ |_|\__,_/_/ /_/ /_/_/ /_/ /_/\__,_/\__, /\___/   *
+*           Software Engineer & FOSS Advicate                        /____/         *
+=====================================================================================
+
+WHO AM I
+=========
+I own and operate a bunch of stuff, the main one being the OneGrid LLC hosting
+company. I also write and run some personal web apps and some server hosting
+philanthropy for open source projects on the side. Im currently a senior in
+high school and I love computers generally. I have taken on a more minimalist
+tech approach so I still have a lot of social media accounts that I just
+abandoned. I tend to just lurk in the shadows mostly. Im an avid linux user
+(ArchLinux, DWM, Friefox, and ST are my go-to tools) but I also dont disrespect
+the windows or mac community. I try to be an all around likeable guy, and you
+can usually find me at makerspaces, makerfaires, or on some IRC/Discord
+servers. If you do see me out in the wild dont shy away from saying hello! I
+love meeting new people. Some other pages you should check out are my <a href="/blog.php">blog</a>
+and my <a href="/quotes.php">quotes book</a>.
+
+WEBSITES
+---------
+  * <a href="https://imnotyourtech.support/">Im not your tech support!</a>
+  * <a href="https://archusers.club"><s style="color: red">Arch Users Club Forum</s></a> Currently down for server migration
+  * <a href="https://postshit.online"><s style="color: red">Post Shit Online</s></a> Currently down for Server Migration
+  * <a href="https://192mb.life">Proof you can run a website for less than 20$/yr</a>
+
+SOCIALS
+--------
+I dont really use social medias, and the ones I have are for my close friends.
+If you really want to talk to me the main place is my email. Emails can be sent
+to <code>hello@rummage.cc</code> and ill get back to them eventually.
+
+If you want to send me <i>encrypted</i> emails, for <i>definetlty not illegal
+purposes</i> then feel free to import my GPG keys and send away!
+<code>curl https://rummage.cc/me.gpg | gpg --import</code>
+<!-- https://www.reddit.com/user/never_the_worst ;)  --!>
+
+</pre>
+
+
+<pre style="position:absolute; bottom:0;">An <a style="color: black; text-decoration: none;" href="https://rummage.cc">Owen Rummage</a> Production</pre>
+    </div>
+  </body>
+</html>
