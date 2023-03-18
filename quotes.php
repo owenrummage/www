7177179353409fd1681977632601d10a5291e684
Owen Rummage <owen@rummage.cc>
2023-03-18 14:37:25 -0500

    Made quotes page look better

--- quotes.php
+++ quotes.php
@@ -5,26 +5,33 @@ $result = getAllQuotes();
 
 <html>
 	<head>
-		<title>Funnie Quotes</title>
-
+		<title>Owen Rummage - Quotes</title>
 	</head>
+
 	<body>
-
-		<pre>
-QUOTES
--------
-These are things ive said, or friends have said that are funny or meaningful
-
-<?php while ($row = $result->fetch_assoc()) {
-    echo '	* "' .
-        $row["Quote"] .
-        '" - ' .
-        $row["Person"] .
-        " (" .
-        $row["Timestamp"] .
-        ")\n";
-} ?>
-		</pre>
-
-	</body>
+		<div style="display:flex;flex-direction:column;width:100%;height:100%;align-items:center;">
+<pre>
+=====================================================================================
+*     ____                         ____                                             *
+*    / __ \_      _____  ____     / __ \__  ______ ___  ____ ___  ____ _____ ____   *
+*   / / / / | /| / / _ \/ __ \   / /_/ / / / / __ `__ \/ __ `__ \/ __ `/ __ `/ _ \  *
+*  / /_/ /| |/ |/ /  __/ / / /  / _, _/ /_/ / / / / / / / / / / / /_/ / /_/ /  __/  *
+*  \____/ |__/|__/\___/_/ /_/  /_/ |_|\__,_/_/ /_/ /_/_/ /_/ /_/\__,_/\__, /\___/   *
+*           Software Engineer & FOSS Advicate                        /____/         *
+=====================================================================================
+</pre>
+<pre>
+  <?php while ($row = $result->fetch_assoc()) {
+      echo '	* "' .
+          $row["Quote"] .
+          '" - ' .
+          $row["Person"] .
+          " (" .
+          $row["Timestamp"] .
+          ")\n";
+  } ?>
+</pre>
+<!--<pre style="position:absolute; bottom:0;">An <a style="color: black; text-decoration: none;" href="https://rummage.cc">Owen Rummage</a> Production</pre>-->
+    </div>
+  </body>
 </html>
