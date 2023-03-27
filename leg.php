e1470490c51e6dd316b5a97e376f27a1db4a49d1
Owen Rummage <owen@rummage.cc>
2023-03-27 18:26:09 -0500

    Re-add Leg

---
+++ leg.php
@@ -0,0 +1,39 @@
+.____     ___________ ________        _______       ____ 
+|    |    \_   _____//  _____/  ___  _\   _  \     /_   |
+|    |     |    __)_/   \  ___  \  \/ /  /_\  \     |   |
+|    |___  |        \    \_\  \  \   /\  \_/   \    |   |
+|_______ \/_______  /\______  /   \_/  \_____  / /\ |___|
+        \/        \/        \/               \/  \/      
+
+OVERVIEW
+=========
+* LEG is an acronym standing for Linux Execution Gang
+* The “Gang” in the name refers to the CPU cores of the processor, as they are a gang of cores that execute Linux and related processes
+* LEG has 256 bit instructions, meaning 256 bit registers
+* LEG will have two different types of firmware (which we call “vibeware”) UEFl (Unified Extendable Firmware LEG), and OpenFirmware
+* The bootloader for UEFl will be an ELF (Executable LEG Format)
+* The file in which UEFl uses is efl/boot/bootleg.efl
+* The first LEG CPU will be released on leg day of 20xx
+* Everytime the LEGStation crashes it makes the “My LEG” sound from SpongeBob Square Pants
+
+STRASCIIADDMULDIVAVG
+=====================
+* This is an added instruction to LEG, it’ll be standard on all LEG CPUs
+    * This instruction will take an an integer and a string
+    * It converts each character of the string to their base 10 ASCII values and all added together
+    * This new value is multiplied by the length of the string
+    * Then this number is divided by the integer
+    * Then we return the average of this new number and the integer
+
+LEGOS
+======
+* LEGOS will only have one supported Window Manager and Desktop Environment
+* LEGDE (pronounced LEG DAY) will combine the best elements of the popular Desktop Environments, it will also have a mobile variant
+* LEGWM will have a floating and tiling version
+
+MIDDLE ENDIAN
+==============
+* We start off with the word. The most significant byte of this word is stored in the middle
+* Then we take the two words that are on either side of the most significant byte
+* We put those words’ most significant bytes in the middle
+* Repeat until the entire word is sorted
